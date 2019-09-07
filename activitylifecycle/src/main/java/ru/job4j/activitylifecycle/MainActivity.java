a97d68095807a7eb84090ab7813c618a0324f2cd
Evgeny Sh <xxx-zet-xxx@yandex.ru>
2019-09-07 18:16:23 +0300

    1. Activity. Жизненный цикл.

---
+++ activitylifecycle/src/main/java/ru/job4j/activitylifecycle/MainActivity.java
@@ -0,0 +1,68 @@
+package ru.job4j.activitylifecycle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = "myLogs";
+    private TextView tvCount;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvCount = findViewById(R.id.tvCount);
+        tvCount.setText("counter " + counter);
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", ++counter);
+        Log.d(LOG_TAG, "onSaveInstanceState counter = " + counter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt("counter");
+        tvCount.setText("counter " + counter);
+        Log.d(LOG_TAG, "onRestoreInstanceState counter = " + counter);
+    }
+}
