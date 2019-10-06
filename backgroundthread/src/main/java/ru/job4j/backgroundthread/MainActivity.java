d267bdc216fc9ece46ad4582a43159c3894b9e11
Evgeny Sh <xxx-zet-xxx@yandex.ru>
2019-10-06 18:09:23 +0300

    03 - AsyncTask

--- backgroundthread/src/main/java/ru/job4j/backgroundthread/MainActivity.java
+++ backgroundthread/src/main/java/ru/job4j/backgroundthread/MainActivity.java
@@ -2,6 +2,7 @@ package ru.job4j.backgroundthread;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,23 +12,24 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 
-import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.net.URL;
-import java.util.Locale;
+import java.net.URLConnection;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "Dev_tag";
-    private Thread testThread;
-    private Thread testRunnable;
-    private TextView tvCount;
-    private ImageView imgV, imgV2;
+    private ImageView imgV;
+    private ProgressBar prBar;
+    private Button btnLoadImg;
     private String[] imgsUrl;
     private AtomicInteger index = new AtomicInteger(0);
 
@@ -39,43 +41,28 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         createUrlList();
-        tvCount = findViewById(R.id.tvCount);
         imgV = findViewById(R.id.imgV);
-        imgV2 = findViewById(R.id.imgV2);
+        prBar = findViewById(R.id.prBarLoadImg);
+        btnLoadImg = findViewById(R.id.btnLoadImg);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
     }
 
     private void createUrlList() {
         this.imgsUrl = new String[]{
                 "http://www.fonstola.ru/pic/201604/2560x1440/fonstola.ru-229435.jpg",
                 "http://www.kartinkijane.ru/pic/201304/1440x900/kartinkijane.ru-16213.jpg",
-                "http://www.hqwallpapers.ru/wallpapers/nature/skala-i-okean.jpg"
+                "http://www.hqwallpapers.ru/wallpapers/nature/skala-i-okean.jpg",
+                "https://w-dog.ru/wallpapers/9/7/496404812485632.jpg",
+                "https://look.com.ua/pic/201209/2560x1440/look.com.ua-45399.jpg"
         };
     }
 
-    public void startThread(View view) {
-        testThread = new TestThread(10);
-        testThread.start();
-        testRunnable = new Thread(new TestRunnable(10));
-        testRunnable.start();
-    }
-
-    public void stopThread(View view) {
-        testThread.interrupt();
-        testRunnable.interrupt();
-    }
-
     public void loadImage(View view) {
-        new Thread(new ImagesLoader(imgV)).start();
-        new Thread(new ImagesLoader(imgV2)).start();
+        if (index.get() >= imgsUrl.length) index.set(0);
+        new ImagesLoaderAsyncTask(this).execute(imgsUrl[index.getAndIncrement()]);
     }
 
     @Override
@@ -94,93 +81,86 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    class ImagesLoader implements Runnable {
-        private final ImageView imageView;
+    private static class ImagesLoaderAsyncTask extends AsyncTask<String, Integer, Bitmap> {
+        private WeakReference<MainActivity> mainActivityWeakReference;
 
-        ImagesLoader(ImageView imageView) {
-            this.imageView = imageView;
+        private ImagesLoaderAsyncTask(MainActivity activity) {
+            this.mainActivityWeakReference = new WeakReference<>(activity);
         }
 
         @Override
-        public void run() {
-            if (index.get() >= imgsUrl.length) index.set(0);
-            final Bitmap img = loadImageFromNetwork(imgsUrl[index.getAndIncrement()]);
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    imageView.setImageBitmap(img);
-                }
-            });
-        }
-
-        private Bitmap loadImageFromNetwork(String url) {
+        protected Bitmap doInBackground(String... strings) {
             Bitmap bitmap = null;
-            try {
-                bitmap = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+            BufferedInputStream is = null;
+            int fileSize, count, total, lastNumber;
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                URL fileUrl = new URL(strings[0]);
+                URLConnection connection = fileUrl.openConnection();
+                connection.connect();
+                fileSize = connection.getContentLength();
+                is = new BufferedInputStream(fileUrl.openStream(), 8192);
+                byte[] data = new byte[4096];
+                count = is.read(data);
+                total = lastNumber = 0;
+                while (count != -1) {
+                    total += count;
+                    baos.write(data, 0, count);
+                    int currentNumber = (total * 100) / fileSize;
+                    if (lastNumber != currentNumber) {
+                        publishProgress(currentNumber);
+                        lastNumber = currentNumber;
+                    }
+                    count = is.read(data);
+                }
+                baos.flush();
+                byte[] array = baos.toByteArray();
+                bitmap = BitmapFactory.decodeByteArray(array, 0, array.length);
             } catch (Exception e) {
                 Log.e(LOG_TAG, e.getMessage());
+            } finally {
+                if (is != null) {
+                    try {
+                        is.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
             return bitmap;
         }
-    }
 
-    class TestThread extends Thread {
-        private int times;
-
-        TestThread(int times) {
-            this.times = times;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            MainActivity activity = mainActivityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+            activity.prBar.setVisibility(View.VISIBLE);
+            activity.btnLoadImg.setClickable(false);
         }
 
         @Override
-        public void run() {
-            getRunMethod(times, getClass().getSimpleName());
-        }
-    }
-
-    class TestRunnable implements Runnable {
-        private int times;
-
-        TestRunnable(int times) {
-            this.times = times;
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            MainActivity activity = mainActivityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+            activity.imgV.setImageBitmap(bitmap);
+            activity.prBar.setVisibility(View.INVISIBLE);
+            activity.prBar.setProgress(0);
+            activity.btnLoadImg.setClickable(true);
         }
 
         @Override
-        public void run() {
-            getRunMethod(times, getClass().getSimpleName());
-        }
-    }
-
-    private void getRunMethod(int times, String sourceName) {
-        int count = 0;
-        while (count != times && !Thread.currentThread().isInterrupted()) {
-            final int finalCount = count;
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    tvCount.setText(String.format(Locale.getDefault(), "%d%%", finalCount * 10));
-                }
-            });
-            Log.d(LOG_TAG, "start" + sourceName + ": " + count);
-            count++;
-            if (Thread.currentThread().isInterrupted()) {
-                Log.d(LOG_TAG, sourceName + " is interrupted");
-                break;
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            MainActivity activity = mainActivityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
             }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                Log.e(LOG_TAG, Arrays.toString(e.getStackTrace()));
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (testThread != null && testRunnable !=null) {
-            testThread.interrupt();
-            testRunnable.interrupt();
+            activity.prBar.setProgress(values[0]);
         }
     }
 }
