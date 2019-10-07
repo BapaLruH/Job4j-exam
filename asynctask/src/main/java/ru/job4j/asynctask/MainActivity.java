3d394f9e0d1924211a5d9cfdca008329d57275ab
Evgeny Sh <xxx-zet-xxx@yandex.ru>
2019-10-07 15:54:55 +0300

    04. RxJava проталкивание и вытягивание.

--- asynctask/src/main/java/ru/job4j/asynctask/MainActivity.java
+++ asynctask/src/main/java/ru/job4j/asynctask/MainActivity.java
@@ -7,53 +7,60 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.lang.ref.WeakReference;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = "DEV_LOG";
     private Button btnStart;
+    private Disposable sbr;
     private ProgressBar prBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        boolean recreated = bundle != null;
+        int progressValue = recreated ? bundle.getInt("progress", 0) : 0;
         btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(v -> startTask(progressValue));
         prBar = findViewById(R.id.prBar);
+        if (recreated) {
+            startTask(progressValue);
+        }
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("progress", prBar.getProgress());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -61,8 +68,36 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void startTask(View view) {
-        new SimpleAsyncTask(this).execute(10);
+    public void startTask(int startValue) {
+        //new SimpleAsyncTask(this).execute(10);
+        int period = prBar.getMax();
+        this.sbr = Observable.interval(period, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .takeUntil(aLong -> aLong.intValue() == period)
+                .doOnSubscribe(disposable -> btnStart.setClickable(false))
+                .doOnComplete(() -> {
+                    btnStart.setClickable(true);
+                    prBar.setProgress(0);
+                    this.sbr.dispose();
+                })
+                .subscribe(v -> prBar.setProgress(startValue + v.intValue()));
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (this.sbr != null && !this.sbr.isDisposed()) {
+            this.sbr.dispose();
+        }
+    }
+
+    public void onClickStop(View view) {
+        if (this.sbr != null && !this.sbr.isDisposed()) {
+            this.sbr.dispose();
+            btnStart.setClickable(true);
+            prBar.setProgress(0);
+        }
     }
 
     private static class SimpleAsyncTask extends AsyncTask<Integer, Integer, String> {
